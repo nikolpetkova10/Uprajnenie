92f3558151bb38574751454ffd12452c11cf2bec
nikolpetkova10 <petkovaniki123@gmail.com>
2026-01-29 13:00:44 +0200

    Add Streamlit app for Python opinion poll

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Мнение за Pythom")
+answer = st.radio("Харесваш ли Python?",("да", "не"))
+
+if answer == "да":
+ st.write("Супер! ")
+else:
+st.write("Няма проблем, ще го харесаш ")
